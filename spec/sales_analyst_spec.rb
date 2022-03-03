9268d370d789434e686bf995c9dad450c3bf0001
Susan Boyd <sueboyd922@gmail.com>
2022-03-02 22:40:40 -0500

    test: add test for creating sales analyst

--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -16,6 +16,11 @@ describe Analyst do
     @sales_analyst = Analyst.new(@sales_engine)
   end
 
+  it 'exists' do
+    se = @sales_engine.analyst
+    expect(se.class).to eq(Analyst)
+  end
+
   it "finds average" do
     expect(@sales_analyst.average_items_per_merchant).to eq(2.88)
   end
